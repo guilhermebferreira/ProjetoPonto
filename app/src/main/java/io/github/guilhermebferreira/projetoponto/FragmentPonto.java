6b783eef01682a125eb7911a1a778a0251cd4a5b
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-20 16:44:31 -0300

    menu criado

--- app/src/main/java/io/github/guilhermebferreira/projetoponto/FragmentPonto.java
+++ app/src/main/java/io/github/guilhermebferreira/projetoponto/FragmentPonto.java
@@ -4,37 +4,33 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class FragmentConfiguracoes extends android.support.v4.app.Fragment {
+public class FragmentPonto extends android.support.v4.app.Fragment {
 
-
-
-    public FragmentConfiguracoes() {
+    public FragmentPonto() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracoes, container, false);
+        return inflater.inflate(R.layout.fragment_ponto, container, false);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
     }
 
     @Override
